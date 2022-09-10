3ca8e47cff61787e8ed8efb50943cea6e90be4b6
Jorge Rivera <jrivera@VESSELARG.local>
2022-09-10 15:59:41 -0300

    functionality complete. Style: incomplete

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
-console.log("app working")
+console.log("app working");
 
+let rating = 0;
 
-let selectBtn = 0;
+function paintOrange(id) {
+  let btns = document.getElementsByClassName("card__number");
 
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].classList.remove("paintOrange");
+  }
 
-function paintOrange(id){         
-      let activeBtn = document.getElementById(id);
-    activeBtn.classList.add("paintOrange")
-
-
-    selectBtn = activeBtn.innerText;
-
-    // let btn = document.getElementsByClassName("card__name");
-    // console.log(btn);
-    // btn.classList.add("paintOrange");
-    
-    
-    // btn.classList.add("card");
-
-    // let activeBtn = document.getElementById(id);
-    // activeBtn.classList.add("paintOrange");
-
-    console.log("boton elegido :" + selectBtn);
+  let activeBtn = document.getElementById(id);
+  activeBtn.classList.add("paintOrange");
+  rating = activeBtn.innerText;
 
 }
 
+function sendRating () {
+    console.log("el rating fue: " + rating)
 
+    let ratingInfo = document.getElementById("ratingInfo");
+    ratingInfo.innerText = "You selected " + rating + " out of 5";
+
+    hideCard();
+
+}
+
+function hideCard (){
+  let firstCard = document.getElementById("firstCard");
+  let secondCard = document.getElementById("secondCard")
+  firstCard.classList.add("hide")
+  secondCard.classList.remove("hide")
+
+
+}
